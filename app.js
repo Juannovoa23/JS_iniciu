2a6365b6d58b2484c63636f567bfa97ac8978852
JuanSe <110542046+Juannovoa23@users.noreply.github.com>
2025-07-31 02:25:46 +0000

    Se modificaron .js and .html

---
+++ app.js
@@ -0,0 +1,9 @@
+alert('Hola mundo');
+let numeroSecreto = 6;
+let numeroDeUsuario = prompt("Indicame un numero por favor: "); 
+console.log(numeroDeUsuario);
+
+if ( numeroDeUsuario == numeroSecreto) { 
+    alert("Acertaste el número");
+}
+
